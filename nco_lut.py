38df92e75da1dcb50c1fa3cf2478f0ae0736dc2c
H-S-S-11 <harry@snell.org.uk>
2020-09-30 02:06:49 +0100

    create function to generate lookup table

--- nco_lut.py
+++ nco_lut.py
@@ -1,8 +1,28 @@
 from nmigen import *
 from nmigen.back.pysim import *
 
+import math
+
+def generate_lookup_table(entry, input_width, output_width):
+    #map the inputs to a normal phase
+    phase = 2.0*math.pi*(entry/(2**input_width))
+    sin_phi = math.sin(phase)
+    #map the output to a range of uints
+    output = int((sin_phi+1)*(2**output_width)/2)
+    return output
+
 class NCO_LUT(Elaboratable):
-    def __init__(self):
-        phi_inc = Signal(32)
+    def __init__(self, sin_input_width, output_width):
+        self.phi_inc_i = Signal(31)
+        self.sine_wave_o = Signal(output_width)
 
     def elaborate(self, platform):
+        m = Module()
+
+        phi = Signal(32)
+        m.d.sync += phi.eq(phi + self.phi_inc_i)
+
+        
+
+        return m
+
