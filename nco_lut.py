e17c19683f897e2cb98c6abc561fcb605307794a
H-S-S-11 <harry@snell.org.uk>
2021-01-04 19:20:56 +0000

    add an option for signed output of nco

--- nco_lut.py
+++ nco_lut.py
@@ -1,12 +1,14 @@
 from nmigen import *
-from nmigen.back.pysim import *
-
+from nmigen.sim import *
 import math
 
-def gen_lookup(entry, input_width, output_width):
+def gen_lookup(entry, input_width, output_width, signed_output=True):
     #map the inputs to a normal phase
     phase = 2.0*math.pi*(entry/((2**input_width)-1))
-    sin_phi = (math.sin(phase)+1)/2
+    if signed_output:
+        sin_phi = math.sin(phase)/2
+    else:
+        sin_phi = (math.sin(phase)+1)/2
     #map the output to a range of uints
     output = int((sin_phi)*((2**output_width)-1))
     return output
@@ -17,16 +19,20 @@ def calc_phi_inc(desired_freq, clock_freq):
     return int(ratio*max_inc)
 
 class NCO_LUT(Elaboratable):
-    def __init__(self, output_width=8, sin_input_width=None):
+    def __init__(self, output_width=8, sin_input_width=None, signed_output=True):
         self.phi_inc_i = Signal(31)
-        self.sine_wave_o = Signal(output_width)
 
+        self.signed_output = signed_output
+        if (signed_output):
+            self.sine_wave_o = Signal(shape=signed(output_width))
+        else:
+            self.sine_wave_o = Signal(shape=unsigned(output_width))
+        
         self.output_width = output_width
         if sin_input_width:
             self.sin_input_width = sin_input_width
         else:
             self.sin_input_width = output_width
-        
 
     def elaborate(self, platform):
         m = Module()
@@ -44,13 +50,13 @@ class NCO_LUT(Elaboratable):
         with m.Switch(table_entry):
             for entry in range(0, 2**input_width):
                 with m.Case(entry):
-                    m.d.sync += sin_o.eq(gen_lookup(entry, input_width, output_width))
+                    m.d.sync += sin_o.eq(gen_lookup(entry, input_width, output_width, signed_output=self.signed_output))
 
         return m
 
 if __name__ == "__main__":
 
-    dut = NCO_LUT(10)
+    dut = NCO_LUT(signed_output=True)
     sim = Simulator(dut)
     sim.add_clock(10e-9) #100MHz
 
