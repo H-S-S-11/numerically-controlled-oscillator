c9d575ecebd0e60a0b91a090e57250606639620f
H-S-S-11 <harry@snell.org.uk>
2021-01-04 18:59:02 +0000

    seperate frequencies for l and r channels

--- tone_synth_ac97_out.py
+++ tone_synth_ac97_out.py
@@ -16,6 +16,7 @@ class Tone_synth(Elaboratable):
         self.pwm_o = Pin(1, "o")
 
         self.phi_inc = calc_phi_inc(tone_frequency, clk_frequency)
+        self.phi_inc_2 = calc_phi_inc(tone_frequency/2, clk_frequency)
 
         self.resolution = resolution
         if pwm_resolution==None:
@@ -26,12 +27,14 @@ class Tone_synth(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        m.submodules.nco = self.nco = nco = NCO_LUT(output_width= self.pwm_resolution, sin_input_width=self.resolution)
+        m.submodules.nco_1 = self.nco_1 = nco_1 = NCO_LUT(output_width= self.pwm_resolution, sin_input_width=self.resolution)
+        m.submodules.nco_2 = self.nco_2 = nco_2 = NCO_LUT(output_width= self.pwm_resolution, sin_input_width=self.resolution)
         m.submodules.ac97 = self.ac97 = ac97 = AC97_Controller()
     
         if(platform != None):
             
-            ac97_if = platform.request("audio_codec")
+            ac97_if = platform.request("audio_codec", 
+                xdr={"sdata_in":2, "sdata_out":0, "audio_sync":0, "reset_o":0})
             ac97.sdata_in = ac97_if.sdata_in
             ac97.sdata_out = ac97_if.sdata_out
             ac97.sync_o = ac97_if.audio_sync
@@ -45,9 +48,10 @@ class Tone_synth(Elaboratable):
 
         zero=Signal(10)
         m.d.comb += [
-            nco.phi_inc_i.eq(self.phi_inc),
-            ac97.dac_channels_i.dac_left_front.eq(Cat(zero, nco.sine_wave_o)),
-            ac97.dac_channels_i.dac_right_front.eq(Cat(zero, nco.sine_wave_o)),
+            nco_1.phi_inc_i.eq(self.phi_inc),
+            nco_2.phi_inc_i.eq(self.phi_inc_2),
+            ac97.dac_channels_i.dac_left_front.eq(Cat(zero, nco_1.sine_wave_o)),
+            ac97.dac_channels_i.dac_right_front.eq(Cat(zero, nco_2.sine_wave_o)),
         ]
 
 
