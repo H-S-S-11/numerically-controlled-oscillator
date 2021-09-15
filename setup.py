3cb3ac7a908464dc3fac90fd50d06a969dd6753a
H-S-S-11 <52609833+H-S-S-11@users.noreply.github.com>
2021-09-15 20:25:55 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="numerically-controlled-oscillator",
-    description="nMigen NCO and other signal processing",
-    python_requires="~=3.6",
-    setup_requires=["wheel", "setuptools", "setuptools_scm"],
-    packages=find_packages(exclude=["tests*", "build"]),
-)
